f1793e5962aa62bbda7b9a088fa448f2495ad3f5
Christian Vetter <christian@rasenplanscher.info>
2019-04-14 06:44:51 +0200

    Make install instructions valid for ranges
    Fixes #26

--- bin.js
+++ bin.js
@@ -10,43 +10,6 @@ const minimist = require("minimist");
 const check = require("./");
 const { PROGRAMS } = require("./");
 
-
-function printInstalledVersion(name, info) {
-  if (info.version) {
-    console.log(name + ": " + chalk.bold(info.version));
-  }
-
-  if (info.notfound) {
-    if (info.isSatisfied) {
-      console.log(chalk.gray(name + ': not installed'));
-    } else {
-      console.error(chalk.yellow.bold(name + ': not installed'));
-    }
-  }
-}
-
-function printVersions(result, print) {
-  Object.keys(PROGRAMS).forEach(name => {
-    const info = result.versions[name];
-    const isSatisfied = info.isSatisfied;
-
-    // report installed version
-    if (print || !isSatisfied) {
-      printInstalledVersion(name, info);
-    }
-
-    // display any non-compliant versions
-    if (!isSatisfied) {
-      const raw = info.wanted.raw;
-      const range = info.wanted.range;
-
-      console.error(chalk.red.bold(`Error: Wanted ${name} version ${raw} (${range}`));
-
-      console.log(chalk.red.bold(PROGRAMS[name].getInstallInstructions(raw)));
-    }
-  });
-}
-
 const argv = minimist(process.argv.slice(2), {
   alias: {
     "print": "p",
@@ -74,12 +37,29 @@ if (argv.help) {
 }
 
 
-let options = Object.keys(PROGRAMS).reduce((memo, name) => ({
+const options = argv.package ? optionsFromPackage() : optionsFromCommandLine();
+
+check(options, (err, result) => {
+  if (err) {
+    throw err;
+  }
+
+  printVersions(result, argv.print);
+  process.exit(result.isSatisfied ? 0 : 1);
+});
+
+
+//
+
+
+function optionsFromCommandLine() {
+  return Object.keys(PROGRAMS).reduce((memo, name) => ({
   ...memo,
   [name]: argv[name],
 }), {});
+}
 
-if (argv.package) {
+function optionsFromPackage() {
   let packageJson;
 
   try {
@@ -98,17 +78,50 @@ if (argv.package) {
     process.exit(1);
   }
 
-  options = Object.keys(PROGRAMS).reduce((memo, name) => ({
+  return Object.keys(PROGRAMS).reduce((memo, name) => ({
     ...memo,
     [name]: packageJson.engines[name],
   }), {});
 }
 
-check(options, (err, result) => {
-  if (err) {
-    throw err;
+
+//
+
+
+function printInstalledVersion(name, info) {
+  if (info.version) {
+    console.log(name + ": " + chalk.bold(info.version));
   }
 
-  printVersions(result, argv.print);
-  process.exit(result.isSatisfied ? 0 : 1);
+  if (info.notfound) {
+    if (info.isSatisfied) {
+      console.log(chalk.gray(name + ': not installed'));
+    } else {
+      console.error(chalk.yellow.bold(name + ': not installed'));
+    }
+  }
+}
+
+function printVersions(result, print) {
+  Object.keys(PROGRAMS).forEach(name => {
+    const info = result.versions[name];
+    const isSatisfied = info.isSatisfied;
+
+    // report installed version
+    if (print || !isSatisfied) {
+      printInstalledVersion(name, info);
+  }
+
+    // display any non-compliant versions
+    if (!isSatisfied) {
+      const raw = info.wanted.raw;
+      const version = raw.replace(/[^.\d]/g, '');
+
+      const range = info.wanted.range;
+
+      console.error(chalk.red.bold(`Error: Wanted ${name} version ${raw} (${range})`));
+
+      console.log(chalk.red.bold(PROGRAMS[name].getInstallInstructions(version)));
+    }
 });
+}
